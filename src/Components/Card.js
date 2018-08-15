0e823d18a2bfe1acaa600cc5b8a0386eb696fb5d
TechnificentConsulting <servicerequest@technificentconsulting.com>
2018-08-15 00:37:14 -0400

    Revert "Working on chat send message"
    This reverts commit f49e2aa4158e10704e6b5672b49c48d2430fa366.

---
+++ src/Components/Card.js
@@ -0,0 +1,11 @@
+import React, { Component
+} from 'react';
+import sage from './sage.svg';
+
+ class Card extends React.Component {
+ 	 render() {
+	 return ( <div className="Card">
+          <img src={sage} className="card-logo" alt="logo" /> <h1 className="card-name">card</h1>
+         </div> );
+  }
+}
--- src/Components/Menu/index.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-
-        render(
-           <div>Testing Menu index</div>
-        );
-
-
