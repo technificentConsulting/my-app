f49e2aa4158e10704e6b5672b49c48d2430fa366
TechnificentConsulting <servicerequest@technificentconsulting.com>
2018-08-15 00:29:03 -0400

    Working on chat send message

---
+++ src/routes/login/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class Login extends Component {
+
+    render(){
+         return(
+             <div className="componentView">
+             
+             <div className="cWindow">Login</div>
+             
+             </div>
+        );
+    }
+}
